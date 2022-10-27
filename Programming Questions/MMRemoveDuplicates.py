0335d29403f1222ca67e784914e738ed8343dcb0
MellowMancer <yatharth.w@somaiya.edu>
2022-10-27 16:47:26 +0530

    Updated readme

--- Programming Questions/MMRemoveDuplicates.py
+++ Programming Questions/MMRemoveDuplicates.py
@@ -5,6 +5,7 @@ n = int(input("Enter number of elements : "))
 for i in range(0, n):
     ele = int(input())
     l.append(ele)
+
 print("Original List: ", l)
 res = [*set(l)]
 print("List after removing duplicate elements: ", res)
