2af38d232a544912cb42141580a77b639322072c
SnappyDragon18 <pg61151@gmail.com>
2022-10-30 03:29:33 +0530

    Create SqrtUsingBinSearchSD.py

---
+++ Programming Questions/SqrtUsingBinSearchSD.py
@@ -0,0 +1,22 @@
+def sqrt(n, p):
+    pres = 10**-p
+    start = 0.0
+    end = n
+    mid = (start + end) * 0.5
+
+    while start < end:
+        mid = (start + end) * 0.5
+        if abs(mid * mid - n) < pres:
+            return mid
+        elif mid * mid < n:
+            start = mid
+        else:
+            end = mid
+
+def main():
+    n = float(input('Enter a number: '))
+    p = int(input('Enter precision: '))
+    print(f'Square root of {n} is {round(sqrt(n, p), p)}')
+
+if __name__=="__main__":
+    main()
